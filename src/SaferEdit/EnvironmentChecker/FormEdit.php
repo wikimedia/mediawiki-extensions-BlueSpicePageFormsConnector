933b959ef7d64bb3e5efdd1d3f4b94575c0ce50d
pwirth <wirth@hallowelt.biz>
2019-11-12 15:13:25 +0100

    Init commit

---
+++ src/SaferEdit/EnvironmentChecker/FormEdit.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace BlueSpice\PageFormsConnector\SaferEdit\EnvironmentChecker;
+
+use BlueSpice\SaferEdit\EnvironmentChecker\Base;
+use Title;
+use SpecialPageFactory;
+
+class FormEdit extends Base {
+
+	public function isEditMode( &$result ) {
+		$action = $this->context->getRequest()->getText( 'action', 'view' );
+
+		if ( $action === 'formedit' && $this->userCanEdit() ) {
+			$result = true;
+			return false;
+		}
+
+		// When editing a page over Special:FormEdit
+		if ( $this->getTitleEditedOverSP() instanceof Title ) {
+			$result = true;
+			return false;
+		}
+		return true;
+	}
+
+	public function shouldShowWarning( &$result ) {
+		$isEdit = false;
+		$this->isEditMode( $isEdit );
+		if ( $isEdit ) {
+			$result = false;
+			return false;
+		}
+		return true;
+	}
+
+	private function getTitleEditedOverSP( $title = null ) {
+		if ( $title === null ) {
+			$title = $this->context->getTitle();
+		}
+		if ( $title->isSpecialPage() ) {
+			$dbKey = $title->getDBkey();
+			$bits =  explode( '/', $dbKey );
+			$specialPageName = array_shift( $bits );
+			if ( $specialPageName ) {
+				$specialTitle = SpecialPageFactory::getTitleForAlias( $specialPageName );
+				if ( !( $specialTitle instanceof Title ) || !$specialTitle->equals( \SpecialPage::getTitleFor( 'FormEdit' ) ) ) {
+					return null;
+				}
+				$page = array_pop( $bits );
+				$pageTitle = Title::newFromText( $page );
+				if ( !( $pageTitle instanceof Title ) || !$pageTitle->exists() ) {
+					return null;
+				}
+				return $pageTitle;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Modify Title object that is being edited
+	 *
+	 * @param Title $title
+	 * @return bool
+	 */
+	public function getEditedTitle( &$title ) {
+		$spTitle = $this->getTitleEditedOverSP( $title );
+		if ( $spTitle instanceof Title ) {
+			$title = $spTitle;
+		}
+		return true;
+	}
+}
