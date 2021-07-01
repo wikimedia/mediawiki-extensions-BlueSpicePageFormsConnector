c3cc8da0d52857ee068fdc385b4ecfb67dd8c700
miriam schlindwein <schlindwein@hallowelt.com>
2021-07-01 10:17:12 +0200

    Fix z-index for datepicker
    Add loading of the style for action formedit
    
    ERM20012
    
    [3.2.x]
    
    Change-Id: I7a6f883ba84d81a3114f3cf94b4aef7955fa4038

--- src/Hook/BeforePageDisplay/AddModuleStyles.php
+++ src/Hook/BeforePageDisplay/AddModuleStyles.php
@@ -4,12 +4,20 @@ namespace BlueSpice\PageFormsConnector\Hook\BeforePageDisplay;
 
 class AddModuleStyles extends \BlueSpice\Hook\BeforePageDisplay {
 
-	protected function doProcess() {
-		$title = $this->skin->getTitle();
-
-		if ( $title->isSpecial( 'FormEdit' ) ) {
-			$this->out->addModuleStyles( 'ext.bluespice.pageformsconnector.styles' );
+	protected function skipProcessing() {
+		$action = $this->out->getRequest()->getText( 'action', 'view' );
+		if ( $action === 'formedit' ) {
+			return false;
 		}
+		if ( $this->out->getTitle()->isSpecial( 'FormEdit' ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	protected function doProcess() {
+		$this->out->addModuleStyles( 'ext.bluespice.pageformsconnector.styles' );
 
 		return true;
 	}
